d8649e746b3b8ea57d6810188fdbb570e9713759
rahul <rahul.cn99@gmail.com>
2021-10-30 10:14:22 +0530

    k

---
+++ src/Age.java
@@ -0,0 +1,20 @@
+
+public class Age {
+private int age;
+private int h;
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public int getH() {
+	return h;
+}
+public void setH(int h) {
+	this.h = h;
+	int a[]= new int[5];
+	a[8]=10;
+}
+
+}
